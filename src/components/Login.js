f211514a6ca017373246777ccf399c2d7f5fc804
Rafal Nowak <rafal.robi.nowak@gmail.com>
2022-10-08 14:19:39 +0200

    Stylizowanie formularzy rejestracji i logowania wraz z niezbędnymi zmianami

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,20 +11,26 @@ const Login = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit(submitForm)} >
-      <h3 className="form-title">Logowanie</h3>
-      <div className="form-group">
-        <label htmlFor="email">Email</label>
-        <EnvelopIcon/>
-        <input type="email" className="input-field" id="email" {...register("email", {required: true})} />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password">Hasło</label>
-        <LockIcon/>
-        <input type="password" className="input-field" id="password" {...register("password", {required: true})} />
-      </div>
-      <button type="submit" className="button">Zaloguj się</button>
-    </form>
+    <div className="form-box">
+      <form onSubmit={handleSubmit(submitForm)} >
+        <h3 className="form-title">Logowanie</h3>
+        <div className="form-group">
+          <label htmlFor="email">E-mail</label>
+          <EnvelopIcon/>
+          <input type="email" className="input-field" id="email" {...register("email", {required: true})} placeholder="piotrkowalski@gmail.com"/>
+          <span></span>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Hasło</label>
+          <LockIcon/>
+          <input type="password" className="input-field" id="password" {...register("password", {required: true})} placeholder="Minimum 8 znaków"/>
+          <span></span>
+        </div>
+        <div className="form-group">
+          <button type="submit" className="form-button">Zaloguj się</button>
+        </div>
+      </form>
+    </div>
   )
 }
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -17,29 +17,33 @@ const Register = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit(submitForm)} >
-      <h3 className="form-title">Zaczynamy</h3>
-      <div className="form-group">
-        <label htmlFor="email">Email</label>
-        <EnvelopIcon/>
-        <input type="email" className="input-field" id="email" {...register("email", {required: true})}/>
-        {/* FIXME: dokończyć spany z informacją o błędach */}
-        <span></span>
-      </div>
-      <div className="form-group">
-        <label htmlFor="password">Hasło</label>
-        <LockIcon/>
-        <input type="password" className="input-field" id="password" {...register("password", {required: true, min: 8})} />
-        <span></span>
-      </div>
-      <div className="form-group">
-        <label htmlFor="passwordConfirm">Powtórz hasło</label>
-        <LockIcon/>
-        <input type="password" className="input-field" id="confirmPassword" {...register("confirmPassword", {required: true, min: 8})} />
-        <span></span>
-      </div>
-      <button type="submit" className="button">zarejestruj się</button>
-    </form>
+    <div className="form-box">
+      <form onSubmit={handleSubmit(submitForm)} >
+        <h3 className="form-title">Zaczynamy!</h3>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <EnvelopIcon/>
+          <input type="email" className="input-field" id="email" {...register("email", {required: true})} placeholder="piotrkowalski@gmail.com"/>
+          {/* FIXME: dokończyć spany z informacją o błędach */}
+          <span></span>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Hasło</label>
+          <LockIcon/>
+          <input type="password" className="input-field" id="password" {...register("password", {required: true, min: 8})} placeholder="Minimum 8 znaków"/>
+          <span></span>
+        </div>
+        <div className="form-group">
+          <label htmlFor="passwordConfirm">Powtórz hasło</label>
+          <LockIcon/>
+          <input type="password" className="input-field" id="confirmPassword" {...register("confirmPassword", {required: true, min: 8})} placeholder="Minimum 8 znaków"/>
+          <span></span>
+        </div>
+        <div className="form-group">
+          <button type="submit" className="form-button">Zarejestruj się</button>
+        </div>
+      </form>
+    </div>
   );
 }
 
